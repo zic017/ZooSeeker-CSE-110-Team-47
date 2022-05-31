7f0eb11266ef0f51293dfb8f7c7871caf27188c1
Kennguyen230 <kennguyen230@gmail.com>
2022-05-30 17:57:31 -0700

    Added popup for empty list

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        getWindow().getDecorView().setBackgroundColor(228B22);
 
         Intent intent = new Intent(this, SearchListActivity.class);
         startActivity(intent);
